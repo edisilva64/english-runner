08a98b67730d22ea1263eb728f9534f25ed390f9
edisilva64 <edisilva64@gmail.com>
2026-09-09 20:20:31 -0300

    Add files via upload

---
+++ data/words.js
@@ -0,0 +1,42 @@
+const WORDS = [
+    {
+        word: "CAT",
+        level: 1
+    },
+    {
+        word: "DOG",
+        level: 1
+    },
+    {
+        word: "SUN",
+        level: 1
+    },
+    {
+        word: "RED",
+        level: 1
+    },
+    {
+        word: "BED",
+        level: 1
+    },
+    {
+        word: "HAT",
+        level: 1
+    },
+    {
+        word: "CAR",
+        level: 1
+    },
+    {
+        word: "PEN",
+        level: 1
+    },
+    {
+        word: "BOOK",
+        level: 1
+    },
+    {
+        word: "FISH",
+        level: 1
+    }
+];
